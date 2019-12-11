e09209e2aa81d1a5fe2b9eb1ed829d79c49e47b4
dbrowski <anthonydombrowski@pingidentity.com>
2019-12-11 11:44:50 -0600

    JWT Decoder Tool

--- src/App.js
+++ src/App.js
@@ -1,26 +1,312 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, lazy, Suspense } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import Popover from "@material-ui/core/Popover";
+import Typography from "@material-ui/core/Typography";
+import LockOpenIcon from "@material-ui/icons/LockOpen";
+import { makeStyles } from "@material-ui/core/styles";
+import * as jwtDecoder from "jwt-js-decode";
+import JSONPretty from "react-json-pretty";
+import JSONPrettyMon from "./App.css";
+// import "react-json-pretty/themes/1337.css";
+// import "react-json-pretty/themes/acai.css";
+// import "react-json-pretty/themes/adventure_time.css";
 
-function App() {
+import { importMDX } from "mdx.macro";
+
+const Content = lazy(() => importMDX("./InfoText.mdx"));
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    minHeight: "100vh",
+    height: "100vh"
+  },
+  image: {
+    backgroundImage:
+      "url(https://pingidentity.com/content/dam/ping-6-2-assets/open-graph-images/2019/P14C-Build-OG.png)",
+    backgroundRepeat: "no-repeat",
+    backgroundColor: "#576877",
+    backgroundSize: "cover",
+    backgroundPosition: "center"
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: "flex",
+    height: "100vh",
+    flexDirection: "column",
+    alignItems: "stretch",
+    justifyContent: "flex-start",
+    color: "#2E4355"
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: "#2E4355"
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(10)
+  },
+  submit: {
+    backgroundColor: "#2E4355",
+    margin: theme.spacing(3, 0, 2)
+  },
+  typography: {
+    color: "#2E4355",
+    fontSize: "1rem"
+  },
+  errorMessage: {
+    color: "red"
+  },
+  infoPaperContainer: {
+    maxHeight: "100%",
+    overflow: "auto"
+  },
+  info: {
+    height: "100%",
+    minHeight: "100%",
+    maxHeight: "100%",
+    color: "#2E4355",
+    margin: "0",
+    padding: "0"
+  }
+}));
+
+export default function App() {
+  // Use the above styles.
+  const classes = useStyles();
+
+  // State variables and setters.
+  const [jot, setJot] = useState("");
+  const [decodedJot, setDecodedJot] = useState("");
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [jotError, setJotError] = React.useState(null);
+
+  const open = Boolean(anchorEl);
+  const id = open ? "popover" : undefined;
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    try {
+      decode();
+    } catch (e) {
+      // Gets the reason for failure.
+      let msg = e.message.split(". ")[1];
+      console.error(msg);
+      setJotError(msg);
+      setAnchorEl(event.currentTarget);
+    }
+  };
+
+  const handleJWTChange = event => {
+    event.preventDefault();
+    setJot(event.target.value);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const decode = () => {
+    let dj = jwtDecoder.jwtDecode(jot);
+    setDecodedJot(dj);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid
+        container
+        item
+        xs={5}
+        direction="column"
+        style={{
+          minHeight: "100vh",
+          maxHeight: "100vh",
+          overflow: "auto"
+        }}
+      >
+        <Grid
+          item
+          spacing={0}
+          className={classes.image}
+          style={{ minHeight: "20%", flex: "1 2 auto" }}
+        ></Grid>
+        <Grid
+          item
+          spacing={0}
+          flex="2 1 auto"
+          style={{
+            maxHeight: "80%",
+            paddingLeft: "15%",
+            paddingRight: "15%",
+            margin: "0",
+            flex: "1 1 auto"
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Suspense fallback={<div>Loading...</div>} id="suspense">
+            <Content id="content" />
+          </Suspense>
+        </Grid>
+      </Grid>
+
+      <Grid
+        item
+        container
+        xs={7}
+        component={Paper}
+        elevation={6}
+        square
+        style={{ height: "100vh" }}
+      >
+        <Grid item container className={classes.paper} direction="column">
+          <Grid
+            item
+            container
+            xs={12}
+            justify="center"
+            style={{
+              flex: "0 1 0"
+            }}
+          >
+            <Avatar className={classes.avatar}>
+              <LockOpenIcon />
+            </Avatar>
+          </Grid>
+          <Grid item xs={12} style={{ flex: "0 1 0" }}>
+            <Typography component="h3" variant="h3" align="center">
+              JWT Decoder
+            </Typography>
+          </Grid>
+          <form className={classes.form} noValidate onSubmit={handleSubmit}>
+            <Grid
+              item
+              container
+              direction="column"
+              justify="flex-between"
+              alignItems="stretch"
+              xs={12}
+              style={{ flex: "10 0 auto" }}
+            >
+              <Grid item xs={12} style={{ flex: "10 0 auto" }}>
+                {/* JWT input field */}
+                <TextField
+                  variant="outlined"
+                  margin="none"
+                  required
+                  fullWidth
+                  id="jwt"
+                  label="JWT"
+                  name="JWT"
+                  value={jot}
+                  autoFocus
+                  rowsMax={4}
+                  multiline
+                  onChange={handleJWTChange}
+                />
+
+                {/* Error Message for JWT String Decode */}
+                <Popover
+                  id={id}
+                  open={open}
+                  anchorEl={anchorEl}
+                  onClose={handleClose}
+                  anchorOrigin={{
+                    vertical: "center",
+                    horizontal: "center"
+                  }}
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "center"
+                  }}
+                >
+                  <Typography className={classes.errorMessage}>
+                    {jotError}
+                  </Typography>
+                </Popover>
+              </Grid>
+              <Grid item xs={12} style={{ flex: "1 0 auto" }}>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  className={classes.submit}
+                >
+                  Decode
+                </Button>
+              </Grid>
+              <Grid item xs={12} style={{ flex: "10 0 auto" }}>
+                <Typography>Header</Typography>
+                <Box
+                  border={1}
+                  borderRadius={5}
+                  borderColor="#576877"
+                  height="100%"
+                  minHeight="10vh"
+                  marginTop="0rem"
+                  marginBottom="1rem"
+                  padding="0"
+                >
+                  {decodedJot ? (
+                    <JSONPretty
+                      id="pretty-header"
+                      data={decodedJot.header}
+                      paddingTop="0"
+                      marginTop="0"
+                      style={{
+                        margin: 0,
+                        padding: 0,
+                        fontSize: "1rem"
+                      }}
+                      mainStyle="padding: 0, margin: 0"
+                      valueStyle="padding: 0, margin: 0"
+                      theme="JSONPretty1337"
+                    />
+                  ) : (
+                    ""
+                  )}
+                </Box>
+              </Grid>
+              <Grid item xs={12} style={{ flex: "10 0 auto" }}>
+                <Typography>Payload</Typography>
+                <Box
+                  border={1}
+                  borderRadius={5}
+                  borderColor="#576877"
+                  height="100%"
+                  minHeight="20vh"
+                  marginTop="0rem"
+                  marginBottom="0rem"
+                >
+                  {decodedJot ? (
+                    <JSONPretty
+                      id="pretty-payload"
+                      data={decodedJot.payload}
+                      theme={JSONPrettyMon}
+                      style={{
+                        fontSize: "1em",
+                        backgroundColor: "#ffffff"
+                      }}
+                      mainStyle="fontSize:1em"
+                      valueStyle="font-size:1em"
+                      booleanStyle="font-size:1em"
+                    />
+                  ) : (
+                    ""
+                  )}
+                </Box>
+              </Grid>
+            </Grid>
+          </form>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
-
-export default App;
