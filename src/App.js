1f846bbc1334e55df2306e33370291eee0c6cffd
Anthony <akdflyinvee73@gmail.com>
2020-10-22 13:12:17 -0500

    Remove unused reference

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
 import Paper from "@material-ui/core/Paper";
 import Box from "@material-ui/core/Box";
