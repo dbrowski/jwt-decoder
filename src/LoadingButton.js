23bc2b6b5de396eaa80722ebb46d955d7e24d17b
Anthony <akdflyinvee73@gmail.com>
2021-05-17 09:55:57 -0500

    dependency updates. removed unused imports

--- src/LoadingButton.js
+++ src/LoadingButton.js
@@ -3,9 +3,6 @@ import clsx from "clsx";
 import { makeStyles } from "@material-ui/core/styles";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import Button from "@material-ui/core/Button";
-import Fab from "@material-ui/core/Fab";
-import CheckIcon from "@material-ui/icons/Check";
-import SaveIcon from "@material-ui/icons/Save";
 
 const useStyles = makeStyles((theme) => ({
   root: {
