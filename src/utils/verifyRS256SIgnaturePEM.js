964cc364c8156f98a2272fd2d2f8b4ce4b2dafdf
Anthony Dombrowski <akdflyinvee73@gmail.com>
2023-06-03 21:14:11 -0500

    fix PEM sig validation error

--- src/utils/verifyRS256SIgnaturePEM.js
+++ src/utils/verifyRS256SIgnaturePEM.js
@@ -4,10 +4,11 @@ import * as rs from "jsrsasign";
 import decodeJWT from "./decodeJWT";
 
 export const verifyRS256SignaturePEM = async (jwt, publicKey) => {
+  const JWS = rs.KJUR.jws.JWS;
   const { headers, claims, sig } = decodeJWT(jwt);
   const pem = publicKey.pem;
   const rsaKey = rs.KEYUTIL.getKey(pem);
-  isValid = JWS.verify(jwt, rsaKey, ["RS256"]);
+  const isValid = JWS.verify(jwt, rsaKey, ["RS256"]);
   return isValid;
 };
 
